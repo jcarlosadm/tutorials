c53af783dd3dfc3da19fc4f0f8bd0c7ac47b8075
jcarlosadm <carlosmaster10@gmail.com>
2014-12-24 15:44:02 -0300

    adicionando pasta employee ao tutorial da caelum

---
+++ java/Caelum/chap4/employee/testEmployee.java
@@ -0,0 +1,77 @@
+
+class Employee{
+    private String name;
+    private String departament;
+    private double salary;
+    private String dateBegin;
+    private String id;
+    
+    public Employee(String name, String departament, double salary, String dateBegin,String id){
+        this.name = name;
+        this.departament = departament;
+        this.salary = salary;
+        this.dateBegin = dateBegin;
+        this.id = id;
+    }
+    
+    public String getName(){
+        return this.name;
+    }
+    
+    public void setName(String name){
+        this.name = name;
+    }
+    
+    public String getDepartament(){
+        return this.departament;
+    }
+    
+    public void setDepartament(String departament){
+        this.departament = departament;
+    }
+    
+    public double getSalary(){
+        return this.salary;
+    }
+    
+    public void setSalary(double salary){
+        this.salary = salary;
+    }
+    
+    public String getDateBegin(){
+        return this.dateBegin;
+    }
+    
+    public void setDateBegin(String dateBegin){
+        this.dateBegin = dateBegin;
+    }
+    
+    public String getId(){
+        return this.id;
+    }
+    
+    public void setId(String id){
+        this.id = id;
+    }
+    
+    public void addBonusSalary(double bonus){
+        this.setSalary(this.getSalary()+bonus);
+    }
+    
+    public double getAnualSalary(){
+        return this.getSalary()*12;
+    }
+}
+
+public class testEmployee{
+    public static void main(String[] args){
+        Employee emp1 = new Employee("Dan","Marketing",1300,"12/12/2013","1423222");
+        
+        emp1.addBonusSalary(300);
+        
+        System.out.print("salário atual: R$ ");
+        System.out.format("%10.2f%n", emp1.getSalary());
+        System.out.print("Salário anual: R$ ");
+        System.out.format("%10.2f%n", emp1.getAnualSalary());
+    }
+}
