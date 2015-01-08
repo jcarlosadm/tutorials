1168cf5de8a403fc891713607d7bd223d35e972c
jcarlosadm <carlosmaster10@gmail.com>
2015-01-08 15:25:31 -0300

    adicionando arquivos do tutorial cap13 da caelum

---
+++ java/Caelum/chap12/src/br/com/bank/DrawException.java
@@ -0,0 +1,8 @@
+package br.com.bank;
+
+@SuppressWarnings("serial")
+public class DrawException extends Exception{
+    public DrawException(String message){
+        super(message);
+    }
+}
---
+++ java/Caelum/chap12/src/br/com/bank/util/Main.java
@@ -0,0 +1,18 @@
+package br.com.bank.util;
+
+import br.com.bank.Account;
+import br.com.bank.DrawException;
+
+public class Main {
+    public static void main(String[] args) {
+        Account account = new Account(2000);
+        
+        try{
+            account.draw(3000);
+        }catch(DrawException e){
+            System.out.println(e.getMessage());
+        }
+        
+        System.out.println(account.getTotal());
+    }
+}
