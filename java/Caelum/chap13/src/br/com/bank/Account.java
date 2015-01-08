29219f4f8ae420d09a8844329496af670c26fd5a
jcarlosadm <carlosmaster10@gmail.com>
2015-01-08 15:47:06 -0300

    gerado javadoc

--- java/Caelum/chap13/src/br/com/bank/Account.java
+++ java/Caelum/chap13/src/br/com/bank/Account.java
@@ -1,12 +1,27 @@
 package br.com.bank;
 
+/**
+ * Classe que representa uma conta do banco
+ */
 public class Account {
+    /**
+     * Saldo da conta
+     */
     private double total;
 
+    /**
+     * Construtor padrão
+     * @param total saldo inicial da conta
+     */
     public Account(double total) {
         this.setTotal(total);
     }
 
+    /**
+     * Saque
+     * @param value Valor a ser sacado
+     * @throws DrawException Classe responsável por lançar erro
+     */
     public void draw(double value) throws DrawException{
         if(this.getTotal() < value)
             throw new DrawException("Saldo insuficiente");
@@ -14,10 +29,18 @@ public class Account {
             this.setTotal(this.getTotal() - value);
     }
 
+    /**
+     * Obtém o saldo da conta
+     * @return retorna o saldo da conta
+     */
     public double getTotal() {
         return total;
     }
 
+    /**
+     * Define o saldo
+     * @param total Saldo da conta
+     */
     private void setTotal(double total) {
         this.total = total;
     }
--- java/Caelum/chap13/src/br/com/bank/DrawException.java
+++ java/Caelum/chap13/src/br/com/bank/DrawException.java
@@ -1,7 +1,13 @@
 package br.com.bank;
-
+/**
+ * Classe que lança execessão ao sacar valor da conta
+ */
 @SuppressWarnings("serial")
 public class DrawException extends Exception{
+    /**
+     * Contrutor padrão
+     * @param message messagem padrão para o erro
+     */
     public DrawException(String message){
         super(message);
     }
