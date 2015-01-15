ada0a63bd7b8c1fce59b41d6235af54e9733241e
jcarlosadm <carlosmaster10@gmail.com>
2015-01-15 11:49:08 -0300

    Adicionando tutorial de leitura de teclado em java

---
+++ java/Caelum/chap15/readKeyborad/src/readKeyboard/Main.java
@@ -0,0 +1,21 @@
+package readKeyboard;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        InputStream is = System.in;
+        InputStreamReader isr = new InputStreamReader(is);
+        BufferedReader br = new BufferedReader(isr);
+        String s = br.readLine();
+        
+        while (s!=null){
+            if(s.equals("end"))break;
+            System.out.println(s);
+            s = br.readLine();
+        }
+    }
+}
