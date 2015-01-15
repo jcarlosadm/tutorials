4ec248fbcc218ac119d959293845551b6c1ea24e
jcarlosadm <carlosmaster10@gmail.com>
2015-01-15 11:56:30 -0300

    tutorial outputstream de java

---
+++ java/Caelum/chap15/writeFile/src/writeFile/Main.java
@@ -0,0 +1,19 @@
+package writeFile;
+
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        OutputStream os = new FileOutputStream("saida");
+        OutputStreamWriter osw = new OutputStreamWriter(os);
+        BufferedWriter bw = new BufferedWriter(osw);
+        
+        bw.write("ola");
+        
+        bw.close();
+    }
+}
