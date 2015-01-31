4b3a33471fcb504b588f18f55f36015255ca3c01
jcarlosadm <carlosmaster10@gmail.com>
2015-01-30 21:12:29 -0300

    adicionado comentários

--- java/JavaFX/bookJavaFx8/chap2/example1_lines/src/com/chapter2/javafxbook/Main.java
+++ java/JavaFX/bookJavaFx8/chap2/example1_lines/src/com/chapter2/javafxbook/Main.java
@@ -15,11 +15,13 @@ import javafx.scene.text.Text;
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
-	    primaryStage.setTitle("Chapter 2: drawing lines");
 	    
+	    // inicialização
+	    primaryStage.setTitle("Chapter 2: drawing lines");
 	    Group root = new Group();
 	    Scene scene = new Scene(root, 300, 300, Color.GRAY);
 	    
+	    // cria linha e adiciona no root
 	    Line line = new Line(50, 100, 250, 100);
         line.setStroke(Paint.valueOf("rgb(255,0,0)"));
         line.setStrokeWidth(10);
@@ -28,22 +30,28 @@ public class Main extends Application {
         line.setStrokeDashOffset(0);
         root.getChildren().add(line);
         
+        // define slider
         Slider slider = new Slider(0, 100, 0);
         slider.setLayoutX(50);
         slider.setLayoutY(200);
         
+        // uma propriedade da linha é controlada pelo slider
         line.strokeDashOffsetProperty().bind(slider.valueProperty());
+        // adiciona slider no root
         root.getChildren().add(slider);
         
+        // define texto
         Text text = new Text("stroke dash offset: 0.0");
         text.setX(50);
         text.setY(170);
         text.setStroke(Color.WHITE);
         
+        // uma propriedade do texto é controlada pelo slider
         slider.valueProperty().addListener((oc,curVal,newVal)->
                 text.setText("stroke dash offset: "+slider.getValue()));
         root.getChildren().add(text);
         
+        // define cena e mostra o palco
         primaryStage.setScene(scene);
         primaryStage.show();
 	}
