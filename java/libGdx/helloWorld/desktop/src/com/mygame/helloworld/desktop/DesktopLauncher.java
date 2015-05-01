180cbbe28ebee0413590b053510771931ed0748c
jcarlos <carlosmaster10@gmail.com>
2015-05-01 11:52:17 -0300

    libgdx tutorial - hello world

---
+++ java/libGdx/helloWorld/desktop/src/com/mygame/helloworld/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygame.helloworld.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygame.helloworld.HelloWorld;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new HelloWorld(), config);
+	}
+}
