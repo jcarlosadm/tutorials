7a1f950ef33029753bfa279a01c1f71bc9a1262d
carlosmaster10 <carlosmaster10@gmail.com>
2015-08-05 12:45:06 -0300

    libgdx simple animation

---
+++ java/libGdx/simpleAnimation/core/src/com/simple/animation/SimpleAnimation.java
@@ -0,0 +1,60 @@
+package com.simple.animation;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+
+public class SimpleAnimation extends ApplicationAdapter {
+	private SpriteBatch batch;
+	private TextureAtlas textureAtlas;
+	private Animation animation;
+	private float elapsedTime = 0;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		textureAtlas = new TextureAtlas(Gdx.files.internal("data/spritesheet.atlas"));
+		animation = new Animation(1/15f, textureAtlas.getRegions());
+	}
+	
+	@Override
+	public void dispose() {
+	    // TODO Auto-generated method stub
+	    batch.dispose();
+	    textureAtlas.dispose();
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		batch.begin();
+		
+		elapsedTime += Gdx.graphics.getDeltaTime();
+		batch.draw(animation.getKeyFrame(elapsedTime, true), 0, 0);
+		
+		batch.end();
+	}
+	
+	@Override
+	public void resize(int width, int height) {
+	    // TODO Auto-generated method stub
+	    super.resize(width, height);
+	}
+	
+	@Override
+	public void pause() {
+	    // TODO Auto-generated method stub
+	    super.pause();
+	}
+	
+	@Override
+	public void resume() {
+	    // TODO Auto-generated method stub
+	    super.resume();
+	}
+}
