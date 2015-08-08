4a3e257af401975918634707a85bc23947ce6e98
jcarlos <carlosmaster10@gmail.com>
2015-08-08 00:16:57 -0300

    libgdx camera basics

---
+++ java/libGdx/camera/desktop/src/com/camera/basics/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.camera.basics.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.camera.basics.CameraBasics;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CameraBasics(), config);
+	}
+}
