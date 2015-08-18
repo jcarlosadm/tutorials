03f76897042be2cb8cc7bba0eda48deaba4dd5e7
jcarlos <carlosmaster10@gmail.com>
2015-08-17 22:52:18 -0300

    libgdx tutorial - tiled basic

---
+++ java/libGdx/tiledBasic/README.md
@@ -0,0 +1,1 @@
+tutorial in http://www.gamefromscratch.com/post/2014/04/16/LibGDX-Tutorial-11-Tiled-Maps-Part-1-Simple-Orthogonal-Maps.aspx
---
+++ java/libGdx/tiledBasic/desktop/src/com/tiled/basic/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.tiled.basic.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.tiled.basic.TiledBasic;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TiledBasic(), config);
+	}
+}
