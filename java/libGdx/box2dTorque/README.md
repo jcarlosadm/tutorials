89df9cc9027cb2538548e010f928aaafeb746b84
jcarlos <carlosmaster10@gmail.com>
2015-08-18 22:22:41 -0300

    libgdx tutorial - torque

---
+++ java/libGdx/box2dTorque/README.md
@@ -0,0 +1,1 @@
+tutorial: http://www.gamefromscratch.com/post/2014/09/10/LibGDX-Tutorial-13-Physics-with-Box2D-Part-2-Force-Impulses-and-Torque.aspx
---
+++ java/libGdx/box2dTorque/desktop/src/com/box/torque/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.box.torque.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.box.torque.BoxTorque;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BoxTorque(), config);
+	}
+}
